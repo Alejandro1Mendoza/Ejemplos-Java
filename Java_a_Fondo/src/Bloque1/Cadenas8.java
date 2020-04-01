d0d3081152f2fa6c9a3a02b9581a1afabfe51bff
laptop <laptop@laptop-de-edwin>
2020-03-31 21:01:53 -0600

    Bloque 1

---
+++ Java_a_Fondo/src/Bloque1/Cadenas8.java
@@ -0,0 +1,25 @@
+package Bloque1;
+
+import java.util.Scanner;
+
+public class Cadenas8 {
+	public static void main(String[] args){
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Ingrese una cadena: ");
+		String s1 = scanner.next();
+		System.out.print("Ingrese otra cadena: ");
+		String s2 = scanner.next();
+		
+		// muestro las cadenas para verifi car lo que contienen
+		System.out.println("s1 = [" + s1 + "]");
+		System.out.println("s2 = [" + s2 + "]");
+		
+		// esto esta mal !!!
+		if( s1 == s2 ){
+			System.out.println("Son iguales");
+		}else{
+			System.out.println("Son distintas");
+		}
+	}
+}
